7fc5cc72cfafc2a751249ee462ea789922f1b9a7
basecamp <jmorgan@basecampcodingacademy.org>
2019-01-04 10:46:24 -0600

    Random tickets are now being printed to the terminal, next step is to be able to choose.

--- Program.java
+++ Program.java
@@ -2,18 +2,44 @@ import java.util.ArrayList;
 
 class Program {
     static Ticket ticket = new Ticket();
+    static int distanceToMars = 62100000;
+    static int minSpeed = 16;
+    static int maxSpeed = 30;
+    static int priceRange = 50 - 36;
+
+    public static String makeTicket(int distanceToMars, int minSpeed, int maxSpeed, ArrayList<String> spacelines) {
+        String spaceline = ticket.randomSpaceline(spacelines);
+        int speed = ticket.randomSpeed(minSpeed, maxSpeed);
+        int days = ticket.getNumberOfDays(speed, distanceToMars);
+        String trip = ticket.randomTrip();
+        int price = ticket.pricePerTrip(trip, speed);
+
+        return String.format("%-18s %-4s  %-10s  $ %s\n", spaceline, days, trip, price);
+
+    }
 
     public static void main(String[] args) {
-        int distanceToMars = 140000000;
-        int minSpeed = 20;
-        int maxSpeed = 35;
 
         ArrayList<String> spacelines = new ArrayList<String>();
         spacelines.add("Space X");
         spacelines.add("Space Adventures");
         spacelines.add("Virgin Galactic");
 
-        System.out.println("Spaceline " + ticket.RandomSpaceline(spacelines));
-        System.out.println("Days: " + ticket.getNumberOfDays(ticket.randomSpeed(minSpeed, maxSpeed), distanceToMars));
+        String table = String.format("%-18s %s  %-10s  %s\n", "Spaceline", "Days", "Trip-type", "Price");
+
+        String bar = "";
+        for (int i = 0; i < table.length(); i++) {
+            bar += "=";
+        }
+
+        table += bar + "\n";
+
+        for (int i = 0; i < 10; i++) {
+            String output = makeTicket(distanceToMars, minSpeed, maxSpeed, spacelines);
+            table += output;
+        }
+
+        System.out.println(table);
+
     }
 }
--- Ticket.java
+++ Ticket.java
@@ -9,13 +9,38 @@ class Ticket {
         return hours / 24;
     }
 
-    public int randomSpeed(int min, int max){
-        return rand.nextInt((max - min)+1) + min;
-    } 
+    public int randomSpeed(int min, int max) {
+        return rand.nextInt((max - min) + 1) + min;
+    }
 
-    public String RandomSpaceline(ArrayList<String> spacelines) {
+    public String randomSpaceline(ArrayList<String> spacelines) {
         Integer index = rand.nextInt(spacelines.size());
         return spacelines.get(index);
+    }
+
+    public String randomTrip() {
+
+        ArrayList<String> tripType = new ArrayList<>();
+        tripType.add("One-way");
+        tripType.add("Round-trip");
+
+        int index = rand.nextInt(tripType.size());
+        return tripType.get(index);
+
+    }
+
+    public Integer pricePerTrip(String trip, int speed) {
+
+        int price = speed + 20;
+
+        if (trip.equals("One-way")) {
+            return price;
+        } else if (trip.equals("Round-trip")) {
+            return price * 2;
+        } else {
+            return null;
+        }
+
     }
 
 }
