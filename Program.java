cd882a30b2acb39e08c5b5cbfdc99b295a671d31
John <jmorgan@basecampcodingacademy.org>
2019-01-14 15:45:11 -0600

    After generating the tickets, prompts users for a ticket id

--- Program.java
+++ Program.java
@@ -1,22 +1,43 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Program {
+    static Scanner reader = new Scanner(System.in);
     static Ticket ticket = new Ticket();
     static int distanceToMars = 62100000;
     static int minSpeed = 16;
     static int maxSpeed = 30;
     static int priceRange = 50 - 36;
 
-    public static String makeTicket(int distanceToMars, int minSpeed, int maxSpeed, ArrayList<String> spacelines) {
+    public static String makeTicket(int distanceToMars, int minSpeed, int maxSpeed, ArrayList<String> spacelines,
+            int id) {
         String spaceline = ticket.randomSpaceline(spacelines);
         int speed = ticket.randomSpeed(minSpeed, maxSpeed);
         int days = ticket.getNumberOfDays(speed, distanceToMars);
         String trip = ticket.randomTrip();
         int price = ticket.pricePerTrip(trip, speed);
 
-        return String.format("%-18s %-4s  %-10s  $ %s\n", spaceline, days, trip, price);
+        return String.format("%-3s %-18s %-4s  %-10s  $ %s\n", id, spaceline, days, trip, price);
 
     }
+
+    public static String chooseTicket(ArrayList<String> tickets) {
+        while (true) {
+            System.out.print("Choose a ticket by Id: ");
+            try {
+                int id = reader.nextInt();
+                if (id < 10) {
+                    return tickets.get(id);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Please enter a valid id.");
+            }
+
+        }
+
+    }
+
 
     public static void main(String[] args) {
 
@@ -25,7 +46,8 @@ class Program {
         spacelines.add("Space Adventures");
         spacelines.add("Virgin Galactic");
 
-        String table = String.format("%-18s %s  %-10s  %s\n", "Spaceline", "Days", "Trip-type", "Price");
+        String table = String.format("%s %-18s %s  %-10s  %s\n", "Id", "Spaceline", "Days", "Trip-type", "Price");
+        ArrayList<String> tickets = new ArrayList<String>();
 
         String bar = "";
         for (int i = 0; i < table.length(); i++) {
@@ -35,11 +57,14 @@ class Program {
         table += bar + "\n";
 
         for (int i = 0; i < 10; i++) {
-            String output = makeTicket(distanceToMars, minSpeed, maxSpeed, spacelines);
+            String output = makeTicket(distanceToMars, minSpeed, maxSpeed, spacelines, i);
             table += output;
+            tickets.add(output);
         }
 
         System.out.println(table);
+        String ticket = chooseTicket(tickets);
+        System.out.println(ticket);
 
     }
 }
